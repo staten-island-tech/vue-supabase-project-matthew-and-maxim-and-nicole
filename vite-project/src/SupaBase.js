536db3ddbff760759e7fe676ac503e36ebd5ea58
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-05-29 13:12:31 -0400

    Create SupaBase.js

---
+++ vite-project/src/SupaBase.js
@@ -0,0 +1,7 @@
+import {createClient} from '@supabase/supabase-js'
+const supabaseURL='ur url'
+const supabaseKey = 'ur anon key'
+const supabase = 'createClient(supabaseURL, supabaseKey)'
+
+export default supabase
+export {supabase}
