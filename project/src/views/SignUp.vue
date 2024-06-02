735e828ba2cfc00f03a0acbdc42b450e58ce3899
nicolew1070 <nicolew88@nycstudents.net>
2024-06-02 18:50:47 -0400

    Create SignUp.vue

---
+++ project/src/views/SignUp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+async function signUp () {
+    const {data: userData, error}  = await supabase.auth.signUp ({
+        email: email.value,
+        username: user.value,
+        password: pass.value,
+    })
+    console.log(userData)
+    if (error) {
+        console.log(error)
+    } else {
+        insertData(userData)
+        return userData
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
