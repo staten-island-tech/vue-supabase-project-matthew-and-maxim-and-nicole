cea0e6951783d4cad59ae0364e56b2fed6fa2838
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-06-06 16:15:09 -0400

    w

---
+++ project/src/components/ItemButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  value: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button @click="$emit('clicked')">{{ value.name }}</button>
+</template>
