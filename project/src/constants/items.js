2c0ebe40004328be0de5c943f4fb656b64745d46
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-06-07 08:41:11 -0400

    Update items.js

--- project/src/constants/items.js
+++ project/src/constants/items.js
@@ -2,5 +2,37 @@ export const ITEMS = [
   {
     name: 'White Bread',
     src: 'https://www.goldmedalbakery.com/content/uploads/2019/12/Jumbo-22oz-White.jpg'
-  }
+  },
+  {
+    name: "Tomato",
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/1200px-Tomato_je.jpg"
+  },
+  {
+    name: "Pickles",
+    src: "https://www.gimmesomeoven.com/wp-content/uploads/2016/07/Homemade-Pickles-Recipe-9.jpg"
+  },
+  {
+    name: "Lettuce",
+    src: "https://www.100daysofrealfood.com/wp-content/uploads/2023/11/vecteezy_lettuce-salad-leaf-isolated-on-white-background-with_5582269-scaled.jpg"
+  },
+  {
+    name: "Peanut Butter",
+    src: "https://hips.hearstapps.com/hmg-prod/images/peanut-butter-vegan-1556206811.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*"
+  },
+  {
+    name: "Jelly",
+    src: "https://p-cdn6consumer.jmsinf.com/tmp/image-thumbnails/smuckers/products/fruit-spreads/image-thumb__5824__auto_feab9401f1ce62dd47e2128655684d97/smckr-strawberry-jelly-18oz-1920x1920.jpg"
+  },
+  {
+    name: "Ketchup",
+    src: "https://images.heb.com/is/image/HEBGrocery/001905374-1"
+  },
+  {
+    name: "Cheddar Cheese",
+    src: "https://131998451.cdn6.editmysite.com/uploads/1/3/1/9/131998451/s174072524441932549_p52_i1_w600.png"
+  },
+  {
+    name: "Bacon",
+    src: "https://www.wellplated.com/wp-content/uploads/2023/06/Baked-bacon-in-the-oven.jpg"
+  },
 ]
