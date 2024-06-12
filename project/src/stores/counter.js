4cf18e8624f63f3095f49ae0ac7d658f8d0715e9
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-06-12 12:36:37 -0400

    Update counter.js

--- project/src/stores/counter.js
+++ project/src/stores/counter.js
@@ -2,14 +2,14 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    cart: [] as {name: string} [],
-    id: '' as String, 
+    cart: [],
+    id: '' 
   }),
   actions: {
     addToCart() {
       this.cart.push(item)
     },
-    insertId(userId: string) {
+    insertId(userId) {
       this.id === (userId)
     }
   },
