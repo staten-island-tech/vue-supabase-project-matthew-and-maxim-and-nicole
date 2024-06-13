feb6012ffe55d48be91f302e7bb1a172b1ebb3bf
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-06-13 08:07:03 -0400

    Update counter.js

--- project/src/stores/counter.js
+++ project/src/stores/counter.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import supabase from '@/supabaseClient';
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
@@ -6,11 +7,56 @@ export const useCartStore = defineStore('cart', {
     id: '' 
   }),
   actions: {
-    addToCart() {
-      this.cart.push(item)
+    async getSavedItems(){ try{
+      const { data: cart, error } = await supabase
+          .from('cart')
+          .select('item');
+          if (error) throw error;
+          this.itemList = cart.map(entry => entry.item);
+        } catch(error) 
+        {
+          console.error( error.message);
+        }
+      },
+      
+      
+      async addItem(item, userID) {
+       /*  itemList.value.push(item);
+        window.scrollTo(
+          0,
+          document.body.scrollHeight || document.documentElement.scrollHeight
+        ); */
+        // use insert rows
+        try {
+          const {data, error } = await supabase
+          .from('cart')
+          .insert([{ item: item.name, user_id: userID}]);
+          if (error) throw error;
+          this.itemList.push(item);
+            window.scrollTo(
+              0,
+              document.body.scrollHeight || document.documentElement.scrollHeight );
+        } catch (error) {
+          console.error( error.message);        
+      }
     },
-    insertId(userId) {
-      this.id === (userId)
-    }
-  },
+      
+      async removeItem(index) {
+       /*  console.log(index);
+        console.log(itemList.value[index]);
+        itemList.value.splice(index, 1); */
+        // use delete rows
+        try {
+          const itemToRemove = itemList.value[index];
+        const {error } = await supabase
+          .from('cart')
+          .delete()
+          .eq('item', itemToRemove);
+        if (error) throw error;
+        this.itemList.splice(index, 1);
+        } catch(error) {
+          console.error(error.message);
+        }
+      },
+    },
 });
