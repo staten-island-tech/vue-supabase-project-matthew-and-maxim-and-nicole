11305b9bd6f2016066d95b9c3db787fa02aba57a
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-06-13 08:08:17 -0400

    Update counter.js

--- project/src/stores/counter.js
+++ project/src/stores/counter.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import supabase from '../supabaseClient';
+import supabase from '../../supabaseClient.js';
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
